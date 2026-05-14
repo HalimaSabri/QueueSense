301f8c47585b35474bab7e4395d2f430e875c013
pc <halimasabri537@gmail.com>
2026-05-14 23:45:05 +0100

    new changes

---
+++ app/Livewire/Admin/ManageServices.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Admin;
+
+use Livewire\Component;
+
+class ManageServices extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.manage-services');
+    }
+}
---
+++ app/Livewire/Client/TakeTicket.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Client;
+
+use Livewire\Component;
+
+class TakeTicket extends Component
+{
+    public function render()
+    {
+        return view('livewire.client.take-ticket');
+    }
+}
